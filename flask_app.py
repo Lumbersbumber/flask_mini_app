2d44636e72e0601637cb520576be742f4d987cbc
Konrad Ullmann <lumbersbumber@gmx.de>
2025-05-15 17:47:19 +0200

    No request to categries

--- flask_app.py
+++ flask_app.py
@@ -2,14 +2,15 @@ from flask import Flask, jsonify, render_template
 import requests
 
 def get_categories():
-    URL= "https://api.chucknorris.io/jokes/categories"
-    response = requests.get(URL)
+    return ["dev", "animal", "food"]
+    # URL= "https://api.chucknorris.io/jokes/categories"
+    # response = requests.get(URL)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f'Fehler: {response.status_code}')
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     return data
+    # else:
+    #     print(f'Fehler: {response.status_code}')
 
 def get_joke_from_api(category):
     URL = "https://api.chucknorris.io/jokes/random?category={}".format(category)
